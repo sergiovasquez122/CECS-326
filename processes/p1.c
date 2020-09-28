95648b3cf6987b31e0e190c3375ea38bf5ba9891
Sergio Vasquez <sergiovasquez122@gmail.com>
2020-09-27 19:04:04 -0700

    added comments to help understanding

--- processes/p1.c
+++ processes/p1.c
@@ -1,17 +1,38 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
+/**
+ * 
+ * What's happening in the program.
+ * Every process is given a PID or process identifier.
+ * In unix the pid is used to name a process in case
+ * you want to do something with the process. for example
+ * killing a process.
+ *
+ * the fork command in unix creates a new process.
+ * the process is almost an exact copy of the old
+ * process but it starts where the fork was called.
+ * also notice that the values of rc are different 
+ * depending on which process is using it. when the
+ * child prints the value of rc we get the integer 0
+ * while when the parent prints the value of rc we 
+ * get the PID of the child. this differentation helps
+ * make the code listed above
+ */
 
 int main(int argc, char *argv[]){
 	printf("hello world (pid:%d)\n", (int) getpid());
 	int rc = fork();
 	if(rc < 0){
+		// fork failed
 		fprintf(stderr, "fork failed\n");
 		exit(1);
 	} else if(rc == 0){
 		printf("hello, I am child (pid:%d)\n", (int) getpid());
+		printf("this is the value of rc: %d\n", rc);
 	} else {
-		printf("hello, I am parent of %d (pid:%d)\n", rc, (int) getpid());
+		printf("hello I am the parent of %d (pid:%d)\n", rc, (int) getpid());
 	}
-	return 0 ;
+	return 0;
 }
+
--- processes/p3.c
+++ processes/p3.c
@@ -4,23 +4,27 @@
 #include <string.h>
 #include <sys/wait.h>
 
-int main(int argc, char* argv[]){
+
+/**
+ */
+int main(int argc, char *argv[]){
 	printf("hello world (pid:%d)\n", (int) getpid());
 	int rc = fork();
 	if(rc < 0){
 		fprintf(stderr, "fork failed\n");
-		exit(1);
+		exit(1);	
 	} else if(rc == 0){
 		printf("hello, I am child (pid:%d)\n", (int) getpid());
-		char *myargs[3];
+		char* myargs[3];
 		myargs[0] = strdup("wc");
 		myargs[1] = strdup("p3.c");
 		myargs[2] = NULL;
 		execvp(myargs[0], myargs);
-		printf("this shouldn't print out");
+		printf("farewell cruel world!"); // this won't print out
 	} else {
 		int rc_wait = wait(NULL);
-		printf("hello, I am parent of %d (rc_wait:%d) (pid:%d)\n", rc, rc_wait, (int) getpid());
+		printf("hello, I am parent of %d (rc_wait(%d) (pid:%d)\n",rc , rc_wait, (int) getpid());
 	}
 	return 0;
 }
+
